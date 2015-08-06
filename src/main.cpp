11b4ed289f01581300f5523f180c5b81bc483397
Marco Gulino <marco@gulinux.net>
2015-08-06 21:53:02 +0200

    save webcam settings between sessions

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+    app.setApplicationName("PlanetaryImager");
+    app.setApplicationDisplayName("Planetary Imager");
+    app.setApplicationVersion("0.1");
     PlanetaryImagerMainWindow mainWindow;
     mainWindow.show();
     return app.exec();
