551e2082d31c58b0433978c6b27bab5e3cca4136
Marco Gulino <marco@gulinux.net>
2015-08-15 12:03:15 +0200

    version from cmake; version bump

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <QApplication>
 #include "planetaryimager_mainwindow.h"
+#include "version.h"
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
     app.setApplicationName("PlanetaryImager");
     app.setApplicationDisplayName("Planetary Imager");
-    app.setApplicationVersion("0.1");
+    app.setApplicationVersion(PLANETARY_IMAGER_VERSION);
     PlanetaryImagerMainWindow mainWindow;
     mainWindow.show();
     return app.exec();
