2ddbba3e01c8922ecc47de4276b7dc669a2123f4
Marco Gulino <marco@gulinux.net>
2015-08-24 01:47:05 +0200

    implemented pixel conversion

--- src/displayimage.cpp
+++ src/displayimage.cpp
@@ -74,13 +74,12 @@ void DisplayImage::handle(const ImageDataPtr& imageData)
 void DisplayImage::create_qimages()
 {
   struct __attribute__ ((__packed__)) Pixel {
-    Pixel() {}
-    Pixel(char gray) : r{gray}, g{gray}, b{gray} {}
-    Pixel(char r, char g, char b) : r{r}, g{g}, b{b} {}
+    void gray(char c) { _r = c; _g = c; _b = c; }
+    void rgb(char r, char g, char b) { _r = r; _g = g; _b = b; }
     const char a = 0xff;
-    char r;
-    char g;
-    char b;
+    char _r;
+    char _g;
+    char _b;
   };
   
   ImageDataPtr imageData;
@@ -91,8 +90,12 @@ void DisplayImage::create_qimages()
     }
     ++d->capture_fps;
     Pixel *pixels = new Pixel[imageData->width() * imageData->height()];
-    if(imageData->channels() == 1) {
-    } else {
+    for(int i=0; i<imageData->width() * imageData->height(); i++) {
+      if(imageData->channels() == 1) {
+        pixels[i].gray(imageData->data()[i]);
+      } else {
+        pixels[i].rgb(imageData->data()[i*3], imageData->data()[i*3]+1, imageData->data()[i*3]+2);
+      }
     }
     QImage image{reinterpret_cast<uint8_t*>(pixels), imageData->width(), imageData->height(), QImage::Format_ARGB32, [](void *data){ delete [] reinterpret_cast<Pixel*>(data); }, pixels};
     d->imageRect = image.rect();
