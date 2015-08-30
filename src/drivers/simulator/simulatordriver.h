50ee7441488437812a2c6d6bef83a28fe90fc4a0
Marco Gulino <marco@gulinux.net>
2015-08-30 20:13:33 +0200

    new simulator driver

---
+++ src/drivers/simulator/simulatordriver.h
@@ -0,0 +1,31 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2015  Marco Gulino <marco.gulino@bhuman.it>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef SIMULATORDRIVER_H
+#define SIMULATORDRIVER_H
+
+#include "drivers/driver.h"
+
+class SimulatorDriver : public Driver
+{
+public:
+virtual Driver::Cameras cameras() const;
+};
+
+#endif // SIMULATORDRIVER_H
