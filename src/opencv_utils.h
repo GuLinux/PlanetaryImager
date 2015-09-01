f5fbfba4e13eb1a4a0c5c3e28746b815677eba03
Marco Gulino <marco@gulinux.net>
2015-09-02 01:14:39 +0200

    implemented sobel&canny edge detection from opencv

--- src/opencv_utils.h
+++ src/opencv_utils.h
@@ -3,22 +3,28 @@
 #include <opencv2/opencv.hpp>
 #include <QDebug>
 
-QDebug operator<<(QDebug dbg, const cv::Point &point) {
+inline QDebug operator<<(QDebug dbg, const cv::Point &point) {
   dbg.nospace() << "{" << point.x << "," << point.y << "}";
   return dbg.space();
 }
 
-QDebug operator<<(QDebug dbg, const cv::Size &size) {
+inline QDebug operator<<(QDebug dbg, const cv::Size &size) {
   dbg.nospace() << "cv::Size{" << size.width << "," << size.height << "}";
   return dbg.space();
 }
 
-QDebug operator<<(QDebug dbg, const cv::Rect &rect) {
+inline QDebug operator<<(QDebug dbg, const cv::Rect &rect) {
   dbg.nospace() << "cv::Rect{" << rect.tl() << "x" << rect.br() << "," << rect.size() << "}";
   return dbg.space();
 }
-QDebug operator<<(QDebug dbg, const cv::Range &range) {
+inline QDebug operator<<(QDebug dbg, const cv::Range &range) {
   dbg.nospace() << "cv::Range{" << range.start << ", " << range.end << "}";
+  return dbg.space();
+}
+inline QDebug operator<<(QDebug dbg, const cv::Mat &mat) {
+  dbg.nospace() << "cv::Mat{" << mat.cols << "x" << mat.rows << ", type=" 
+    << mat.type() << ", depth=" << mat.depth() << ", channels=" << mat.channels()
+    << ", valid=" << (mat.data == nullptr ? "false" : "true");
   return dbg.space();
 }
 #endif
