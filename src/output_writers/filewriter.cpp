1ac703c9be920d6c5d82615ff94352e5a7b15541
Marco Gulino <marco@gulinux.net>
2016-09-18 21:01:37 +0100

    More cleanup

--- src/output_writers/filewriter.cpp
+++ src/output_writers/filewriter.cpp
@@ -27,8 +27,6 @@ QMap< Configuration::SaveFormat, FileWriter::Factory > FileWriter::factories()
 {
   return {
     {Configuration::SER, [](const QString &deviceName, Configuration &configuration){ return make_shared<SERWriter>(deviceName, configuration); }},
-#ifdef HAVE_CV_VIDEO
     {Configuration::Video, [](const QString &deviceName, Configuration &configuration){ return make_shared<cvVideoWriter>(deviceName, configuration); }},
-#endif
   };
 }
