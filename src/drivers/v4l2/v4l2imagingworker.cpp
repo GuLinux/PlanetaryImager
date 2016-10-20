9849b3a585bb7d688ece24fcc90031b80c65af28
Marco Gulino <marco@gulinux.net>
2016-10-20 19:06:00 +0100

    Forwardport refactoring for multiple formats

--- src/drivers/v4l2/v4l2imagingworker.cpp
+++ src/drivers/v4l2/v4l2imagingworker.cpp
@@ -22,8 +22,12 @@
 #include "v4l2buffer.h"
 #include "v4l2utils.h"
 #include <QDebug>
-
+#include <QHash>
+#include <functional>
+#include "v4l2exception.h"
+#include "c++/stringbuilder.h"
 using namespace std;
+using namespace std::placeholders;
 
 
 
@@ -34,11 +38,25 @@ DPTR_IMPL(V4L2ImagingWorker) {
   uint32_t bufferinfo_type;
   void adjust_framerate();
   int request_buffers(int count);
+  function<Frame::ptr(const V4LBuffer::ptr)> get_frame;
+  Frame::ptr import_frame(const V4LBuffer::ptr &buffer);
+  Frame::ptr create_frame(const V4LBuffer::ptr &buffer, int cv_type, Frame::ColorFormat color_format);
+  Frame::ptr convert_frame(const V4LBuffer::ptr &buffer, int cv_type, int cv_conversion_format, Frame::ColorFormat color_format);
 };
 
 V4L2ImagingWorker::V4L2ImagingWorker(const V4L2Device::ptr& device, const v4l2_format& format) : dptr(device, format)
 {
-  qDebug() << "Starting v4l2 worker with format=" << FOURCC2QS(format.fmt.pix.pixelformat) << ", res=" << "%1x%2"_q % format.fmt.pix.width % format.fmt.pix.height;
+  QHash<uint32_t, function<Frame::ptr(const V4LBuffer::ptr)>> formats = {
+    {V4L2_PIX_FMT_GREY, bind(&Private::create_frame, d.get(), _1, CV_8UC1, Frame::Mono)},
+    {V4L2_PIX_FMT_MJPEG, bind(&Private::import_frame, d.get(), _1)},
+    {V4L2_PIX_FMT_YUYV, bind(&Private::convert_frame, d.get(), _1, CV_8UC2, CV_YUV2RGB_YUYV, Frame::RGB)},
+  };
+  auto pixelformat = format.fmt.pix.pixelformat;
+  if(!formats.contains(pixelformat))
+    throw V4L2Exception(V4L2Exception::unimplemented_error, GuLinux::stringbuilder() << "Requested format " << FOURCC2QS(pixelformat).toStdString() << " (" << pixelformat << ") not yet supported. Please report this error message for help");
+  d->get_frame = formats[pixelformat];
+  
+  qDebug() << "Starting v4l2 worker with format=" << FOURCC2QS(pixelformat) << ", res=" << "%1x%2"_q % format.fmt.pix.width % format.fmt.pix.height;
   d->adjust_framerate();
   int buffers = d->request_buffers(4);
   for(uint32_t i=0; i< buffers; i++) {
@@ -102,24 +120,32 @@ Frame::ptr V4L2ImagingWorker::shoot()
   auto buffer = d->buffers.dequeue(d->device);
   BENCH_END(dequeue_buffer);
   QBENCH(decode_image)->every(100)->ms();
-  cv::Mat image{static_cast<int>(d->format.fmt.pix.height), static_cast<int>(d->format.fmt.pix.width), CV_8UC3};
-  if(d->format.fmt.pix.pixelformat == V4L2_PIX_FMT_MJPEG) {
-      color_format = Frame::BGR;
-      cv::InputArray inputArray{buffer->bytes(),  static_cast<int>(buffer->size())};
-      image = cv::imdecode(inputArray, -1);
-  } else if(d->format.fmt.pix.pixelformat == V4L2_PIX_FMT_YUYV) {
-      cv::Mat source{static_cast<int>(d->format.fmt.pix.height), static_cast<int>(d->format.fmt.pix.width), CV_8UC2, buffer->bytes() };
-      cv::cvtColor(source, image, CV_YUV2RGB_YUYV);
-  } else if(d->format.fmt.pix.pixelformat == V4L2_PIX_FMT_GREY) {
-    image = cv::Mat{static_cast<int>(d->format.fmt.pix.height), static_cast<int>(d->format.fmt.pix.width), CV_8UC1};
-    copy(buffer->bytes(), buffer->bytes() + buffer->size(), image.begin<uint8_t>());
-    color_format = Frame::Mono;
-  } else {
-      qCritical() << "Unsupported image format: " << FOURCC2QS(d->format.fmt.pix.pixelformat);
-      return {}; // TODO: throw exception?
-  }
+
+  auto frame = d->get_frame(buffer);
   BENCH_END(decode_image);
   buffer->queue(); // TODO: other types?
+  return frame;
+}
+
+Frame::ptr V4L2ImagingWorker::Private::import_frame(const V4LBuffer::ptr& buffer)
+{
+  cv::InputArray inputArray{buffer->bytes(),  static_cast<int>(buffer->size())};
+  cv::Mat image = cv::imdecode(inputArray, -1);
+  return make_shared<Frame>(Frame::BGR, image);
+}
+
+
+Frame::ptr V4L2ImagingWorker::Private::convert_frame(const V4LBuffer::ptr& buffer, int cv_type, int cv_conversion_format, Frame::ColorFormat color_format)
+{
+  cv::Mat image{static_cast<int>(format.fmt.pix.height), static_cast<int>(format.fmt.pix.width), cv_type, buffer->bytes()};
+  cv::cvtColor(image, image, cv_conversion_format);
   return make_shared<Frame>(color_format, image);
+}
+
+Frame::ptr V4L2ImagingWorker::Private::create_frame(const V4LBuffer::ptr& buffer, int cv_type, Frame::ColorFormat color_format)
+{
+    cv::Mat image = cv::Mat{static_cast<int>(format.fmt.pix.height), static_cast<int>(format.fmt.pix.width), cv_type};
+    copy(buffer->bytes(), buffer->bytes() + buffer->size(), image.begin<uint8_t>());
+    return make_shared<Frame>(color_format, image);
 }
 
