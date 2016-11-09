41a37114ccf7836d63d83822bf5cd75f2f753cfd
Marco Gulino <marco@gulinux.net>
2016-11-09 18:51:36 +0000

    Base for daemon application

---
+++ src/planetaryimager_daemon.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2016  Marco Gulino <marco@gulinux.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include <QCoreApplication>
+#include "planetaryimager_mainwindow.h"
+#include "commons/version.h"
+#include <iostream>
+#include <iomanip>
+#include <QDebug>
+#include "c++/backtrace.h"
+#include <unistd.h>
+#include <signal.h>
+using namespace std;
+
+
+void crash_handler(int sig) {
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  cerr << GuLinux::Backtrace::backtrace(50, 1);
+  exit(1);
+}
+
+void log_handler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+  static map<QtMsgType, string> log_levels {
+    {QtFatalMsg   , "FATAL"},
+    {QtCriticalMsg, "CRITICAL"},
+    {QtWarningMsg , "WARNING"},
+    {QtDebugMsg   , "DEBUG"},
+  };
+  QString position;
+  if(context.file && context.line) {
+    position = QString("%1:%2").arg(context.file).arg(context.line).replace(SRC_DIR, "");
+  }
+  QString function = context.function ? context.function : "";
+  cerr << setw(8) << log_levels[type] << " - " /*<< qPrintable(position) << "@"*/<< qPrintable(function) << " " << qPrintable(msg) << endl;
+  cerr.flush();
+}
+
+int main(int argc, char** argv)
+{
+    signal(SIGSEGV, crash_handler);
+    signal(SIGABRT, crash_handler);
+    cerr << "Starting PlanetaryImager Daemon - version " << PLANETARY_IMAGER_VERSION << " (" << HOST_PROCESSOR << ")" << endl;
+    QCoreApplication app(argc, argv);
+    qInstallMessageHandler(log_handler);
+    app.setApplicationName("PlanetaryImager");
+    app.setApplicationVersion(PLANETARY_IMAGER_VERSION);
+    // TODO: initialize all handlers and drivers here
+    //PlanetaryImagerMainWindow mainWindow;
+    //mainWindow.show();
+    return app.exec();
+}
