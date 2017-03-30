50ea92f5dd917d8830f7fac2d3ac7486520439d2
Marco Gulino <marco@gulinux.net>
2017-03-30 19:30:55 +0100

    Various build fixes

---
+++ support/docker-builds/images/debian-based-commons/configuration-debian-testing.cmake
@@ -0,0 +1,4 @@
+set(CPACK_GENERATOR DEB)
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libusb-1.0-0, libqt5gui5 (>= 5.5.0), libqt5network5 (>= 5.5.0), libqt5widgets5 (>= 5.5.0), libopencv-imgproc2.4v5, libopencv-core2.4v5, libopencv-highgui2.4v5, fxload, libccfits0v5")
+set(EXTRA_LIBRARIES udev)
+
