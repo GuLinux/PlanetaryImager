3bdfd8a99d6c1346571ad30cef9404646ceb89bc
Marco Gulino <marco@gulinux.net>
2017-04-02 12:29:11 +0100

    Cross compilation environment fixes

--- support/docker-builds/images/debian-based-commons/configuration-ubuntu-16.10.cmake
+++ support/docker-builds/images/debian-based-commons/configuration-ubuntu-16.10.cmake
@@ -1,4 +1,2 @@
-set(CPACK_GENERATOR DEB)
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libusb-1.0-0, libqt5gui5 (>= 5.5.0), libqt5network5 (>= 5.5.0), libqt5widgets5 (>= 5.5.0), libopencv-imgproc2.4v5, libopencv-core2.4v5, libopencv-highgui2.4-deb0, fxload, libccfits0v5")
 set(EXTRA_LIBRARIES udev)
-set(cflags_fpic Off)
