a55af85c9022f2ae0915b18e7c97fda0dd66fe7d
Marco Gulino <marco@gulinux.net>
2017-04-20 21:37:42 +0100

    Restore QHY driver by default, using RPATH to link to libqhy

--- src/drivers/qhy/qhydriver.cpp
+++ src/drivers/qhy/qhydriver.cpp
@@ -50,13 +50,11 @@ public:
 QHYDriver::QHYDriver() : dptr(this)
 {
   QHY_CHECK << InitQHYCCDResource() << "initializing QHY Driver";
-  //qDebug() << "Initialized QHY Driver";
 }
 
 QHYDriver::~QHYDriver()
 {
   QHY_CHECK << ReleaseQHYCCDResource() << "releasing QHY Driver";
-  //qDebug() << "Released QHY Driver";
 }
 
 
