1269260ae1b1cc5fafef0230835e9230bcd42e3a
Marco Gulino <marco@gulinux.net>
2017-04-23 11:12:22 +0100

    Small progress on osx packaging

--- support/docker-builds/images/osx/osxcross-toolchain.cmake
+++ support/docker-builds/images/osx/osxcross-toolchain.cmake
@@ -23,31 +23,30 @@ set(CMAKE_INSTALL_RPATH ...)
 #set(CMAKE_AR /usr/bin/ar CACHE FILEPATH "Archiver")
 set(AM_QT_MOC_EXECUTABLE "/usr/lib/x86_64-linux-gnu/qt5/bin/moc")
 set(QT_MOC_EXECUTABLE ${AM_QT_MOC_EXECUTABLE})
-add_executable(Qt5::moc IMPORTED)
+if(NOT TARGET Qt5::moc) 
+    add_executable(Qt5::moc IMPORTED)
+endif()
 set_property(TARGET Qt5::moc PROPERTY IMPORTED_LOCATION ${QT_MOC_EXECUTABLE})
 
 set(AM_QT_UIC_EXECUTABLE "/usr/lib/x86_64-linux-gnu/qt5/bin/uic")
 set(QT_UIC_EXECUTABLE ${AM_QT_UIC_EXECUTABLE})
-add_executable(Qt5::uic IMPORTED)
+if(NOT TARGET Qt5::uic)
+    add_executable(Qt5::uic IMPORTED)
+endif()
 set_property(TARGET Qt5::uic PROPERTY IMPORTED_LOCATION ${QT_UIC_EXECUTABLE})
 
 
 set(AM_QT_RCC_EXECUTABLE "/usr/lib/x86_64-linux-gnu/qt5/bin/rcc")
 set(QT_RCC_EXECUTABLE ${AM_QT_RCC_EXECUTABLE})
-add_executable(Qt5::rcc IMPORTED)
+if(NOT TARGET Qt5::rcc)
+    add_executable(Qt5::rcc IMPORTED)
+endif()
 set_property(TARGET Qt5::rcc PROPERTY IMPORTED_LOCATION ${QT_RCC_EXECUTABLE})
 
-#set(CCFITS_LIBRARY /opt/ccfits/lib/libCCfits.dylib)
-include_directories(
-		    /opt/Qt5-includes/
-		    /opt/ccfits/include/
-		    /opt/Qt5-includes/QtCore
-#		    /osxcross/target/SDK/MacOSX10.11.sdk/usr/include/c++/v1
-#		    /osxcross/target/SDK/MacOSX10.11.sdk/usr/include/c++/4.2.1
-#		    /osxcross/target/SDK/MacOSX10.11.sdk/usr/include
-)
-
-set(CMAKE_PREFIX_PATH "/osxcross/target/macports/pkgs/opt/local/libexec/qt5/;/osxcross/target/macports/pkgs/opt/local")
+set(CCFITS_LIBRARY_PATH /opt/CCfits/lib/ CACHE PATH "CCfits find_library include path")
+include_directories(/opt/CCfits/include/)
+link_directories(/opt/CCfits/lib)
+set(CMAKE_PREFIX_PATH "/osxcross/target/macports/pkgs/opt/local/libexec/qt5/;/osxcross/target/macports/pkgs/opt/local;/opt/CCfits;/opt/cfitsio")
 
 
 # run with -DCMAKE_TOOLCHAIN_FILE=/osxcross-toolchain.cmake
