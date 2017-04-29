39d3678502153971c0224759b0f2b8c68a515f91
Marco Gulino <marco@gulinux.net>
2017-04-29 23:33:09 +0100

    Fix qhy header-fixing tool

--- src/drivers/qhy/qhy_fix_libusb_include.py
+++ src/drivers/qhy/qhy_fix_libusb_include.py
@@ -5,7 +5,7 @@ import fileinput
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-with fileinput.FileInput(input_file) as file:
+with open(input_file, 'r') as file:
   with open(output_file, 'w') as output:
     for line in file:
       output.write(line.replace('#include <libusb-1.0/libusb.h>', '#include <libusb.h>'))
