e3cf9f8da6d39af5a47da8db915a5ff5bc3bc45a
Marco Gulino <marco@gulinux.net>
2017-10-17 21:24:13 +0100

    New API for configuration entries

--- scripting_client/planetaryimager/configuration.py
+++ scripting_client/planetaryimager/configuration.py
@@ -1,5 +1,6 @@
 from .network import *
 
+
 class Configuration:
     """Enumerates, get and set configuration values of Planetary Imager
 
@@ -7,33 +8,72 @@ class Configuration:
     Note that some configuration items are C++ enums, which are translated in the network protocol as numbers.
     Refer to `src/commons/configuration.h` in PlanetaryImager to see the allowed values for enum entries.
     """
+
+    class Entry:
+        """Class representing a configuration entry"""
+        def __init__(self, name, type, client):
+            """This entry name."""
+            self.name = name
+            """The type in PlanetaryImager (QString, int, an enum type, etc)."""
+            self.type = type
+            self.client = client
+
+        @property
+        def value(self):
+            """Get the current entry value"""
+            return ConfigurationProtocol.get(self.client, self.name)['value']
+
+        @value.setter
+        def value(self, value):
+            """Set a new configuration value for this entry.
+
+            :param value: new value to set.
+            """
+            ConfigurationProtocol.set(self.client, self.name, value)
+
+        def reset(self):
+            """Reset this entry to its default configuration value."""
+            ConfigurationProtocol.get(self.client, self.name)
+
+        def __str__(self):
+            return '{} ({}) = {}'.format(self.name, self.type, self.value)
+
+        def __repr__(self):
+            return self.__str__()
+
     def __init__(self, client):
         self.client = client
+        self.__entries = None
 
-    def list(self):
+    @property
+    def entries(self):
         """List the available configuration entries.
 
         :return: a dictionary with the configuration key names as keys, and the type as values.
         """
-        return ConfigurationProtocol.list(self.client)
+        if not self.__entries:
+            self.__entries = {}
+            for name, type in ConfigurationProtocol.list(self.client).items():
+                self.__entries[name] = Configuration.Entry(name, type, self.client)
+        return self.__entries
 
-    def get(self, name):
-        """Retrieve the current configuration value for `name`.
+    @property
+    def keys(self):
+        """List all the configuration entries by name"""
+        return list(self.entries.keys())
+
+    def entry(self, name):
+        """Retrieve the current configuration object for `name`.
 
         :param name: the configuration item name
-        :return: a dictionary populated with configuration `name` and `value`.
+        :return: a Configuration.Entry object.
         """
-        return ConfigurationProtocol.get(self.client, name)
+        return self.entries[name]
 
-    def set(self, name, value):
-        """Set a new configuration value for `name`.
+    def __getattr__(self, item):
+        """Access a configuration item as an attribute (configuration.entry_name)"""
+        return self.entries[item] if item in self.entries else None
 
-        :param name: name of the configuration item to set.
-        :param value: new value to set.
-        """
-        ConfigurationProtocol.set(self.client, name, value)
-
-    def reset(self, name):
-        """Reset `name` to its default configuration value."""
-        ConfigurationProtocol.reset(self.client, name)
-
+    def __getitem__(self, item):
+        """Access a configuration item as a dictionary element (configuration['entry_name'])"""
+        return self.entries[item]
