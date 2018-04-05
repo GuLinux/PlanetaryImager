41447f78956709fd3bed5cee1b592585474095c9
Marco Gulino <marco@gulinux.net>
2018-04-05 22:33:50 +0100

    Update support for ubuntu 17.10

---
+++ support/docker-builds/images/debian-based-commons/configuration-ubuntu-17.10.cmake
@@ -0,0 +1,2 @@
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libusb-1.0-0, libqt5gui5 (>= 5.5.0), libqt5opengl5 (>= 5.5.0), libqt5network5 (>= 5.5.0), libqt5widgets5 (>= 5.5.0), libopencv-imgproc3.1, libopencv-core3.1, libopencv-highgui3.1, fxload, libccfits0v5")
+set(EXTRA_LIBRARIES udev)
