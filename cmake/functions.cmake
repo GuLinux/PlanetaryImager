6febaed14f19ae7bf58e03f3a3249a113bd823af
Marco Gulino <marco@gulinux.net>
2019-06-03 21:41:41 +0100

    Add CMake function to easily download SDK into source code

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -63,3 +63,64 @@ function(external_project_download IN_FILE OUT_DIR)
   endif()
 endfunction()
 
+function(add_sdk)
+  # Sets the following variables into cache:
+  # ${NAME}_SDK_DIR
+  # ${NAME}_SDK_VERSION
+  # ${NAME}_SDK_URL
+
+  set(options "")
+  set(oneValueArgs NAME MAJOR MINOR PATCH SUB URL HASH HASH_ALGO)
+  set(multiValueArgs "")
+  cmake_parse_arguments(add_sdk "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  set(SDK_VERSION "${add_sdk_MAJOR}.${add_sdk_MINOR}.${add_sdk_PATCH}${add_sdk_SUB}")
+  set(SDK_DIR "${CMAKE_CURRENT_SOURCE_DIR}/${add_sdk_NAME}_v${SDK_VERSION}")
+  set(${add_sdk_NAME}_SDK_DIR "${SDK_DIR}" CACHE STRING "${add_sdk_NAME} SDK Directory")
+  set(${add_sdk_NAME}_SDK_VERSION "${SDK_VERSION}" CACHE STRING "${add_sdk_NAME} SDK Version")
+
+  string(CONFIGURE "${add_sdk_URL}" SDK_URL)
+
+  set(${add_sdk_NAME}_SDK_URL ${SDK_URL} CACHE STRING "${add_sdk_NAME} SDK URL")
+  get_filename_component(SDK_ARCHIVE "${SDK_URL}" NAME)
+  set(SDK_ARCHIVE "${CMAKE_CURRENT_BINARY_DIR}/${SDK_ARCHIVE}")
+  
+  message("Adding `${add_sdk_NAME}` SDK: `${SDK_DIR}` `${SDK_URL}` `${SDK_ARCHIVE}`")
+
+  if("${SDK_ARCHIVE}" MATCHES ".tar.bz2$")
+    set(EXTRACT_COMMAND tar)
+    set(EXTRACT_COMMAND_ARGS xjf ${SDK_ARCHIVE})
+  elseif("${SDK_ARCHIVE}" MATCHES ".tar.gz$")
+    set(EXTRACT_COMMAND tar)
+    set(EXTRACT_COMMAND_ARGS xzf ${SDK_ARCHIVE})
+  elseif("${SDK_ARCHIVE}" MATCHES ".zip$")
+    set(EXTRACT_COMMAND unzip)
+    set(EXTRACT_COMMAND_ARGS ${SDK_ARCHIVE})
+  else()
+    message(FATAL_ERROR "${SDK_ARCHIVE}: Unrecognized archive format")
+  endif()
+
+  if(NOT EXISTS ${SDK_DIR})
+    message(STATUS "Downloading ${add_sdk_NAME} SDK v${SDK_VERSION}")
+    file(DOWNLOAD
+          "${SDK_URL}"
+          "${SDK_ARCHIVE}"
+          EXPECTED_HASH ${add_sdk_HASH_ALGO}=${add_sdk_HASH}
+          SHOW_PROGRESS LOG SDK_DOWNLOAD_LOG STATUS SDK_DOWNLOAD_STATUS
+    )
+  
+    list(LENGTH SDK_DOWNLOAD_STATUS SDK_DOWNLOAD_STATUS_LENGTH)
+    list(GET SDK_DOWNLOAD_STATUS 0 SDK_DOWNLOAD_STATUS_CODE)
+    list(GET SDK_DOWNLOAD_STATUS 1 SDK_DOWNLOAD_STATUS_MESSAGE)
+    if(${SDK_DOWNLOAD_STATUS_CODE})
+      message(FATAL_ERROR "Error while downloading ${add_sdk_NAME} SDK: ${SDK_DOWNLOAD_STATUS_MESSAGE}(code: ${SDK_DOWNLOAD_STATUS_MESSAGE}. Full log: ${SDK_DOWNLOAD_LOG}")
+    endif()
+    file(MAKE_DIRECTORY "${SDK_DIR}")
+    execute_process(COMMAND ${EXTRACT_COMMAND} ${EXTRACT_COMMAND_ARGS} WORKING_DIRECTORY ${SDK_DIR} RESULT_VARIABLE UNPACK_ERROR)
+    if(NOT ${UNPACK_ERROR} EQUAL 0)
+      file(REMOVE_RECURSE ${SDK_DIR})
+      message(FATAL_ERROR "Error unpacking ${add_sdk_NAME} SDK: ${UNPACK_ERROR}")
+    endif()
+  endif()
+
+
+endfunction()
