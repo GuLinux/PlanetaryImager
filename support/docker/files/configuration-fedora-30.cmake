eaf1be1d7c11245144c316e429c0960a640d71e3
Marco Gulino <marco@gulinux.net>
2019-06-09 17:17:45 +0100

    Remove unused fxload and libusb dependencies

--- support/docker/files/configuration-fedora-30.cmake
+++ support/docker/files/configuration-fedora-30.cmake
@@ -1,3 +1,3 @@
 include(${CMAKE_CURRENT_LIST_DIR}/configuration-fedora-base.cmake)
-set(CPACK_RPM_PACKAGE_REQUIRES "qt5-qtbase >= 5.5.0, fxload, CCfits, libdc1394" CACHE STRING "")
+set(CPACK_RPM_PACKAGE_REQUIRES "qt5-qtbase >= 5.5.0, CCfits, libdc1394" CACHE STRING "")
 
