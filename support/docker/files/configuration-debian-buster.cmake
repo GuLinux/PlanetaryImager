0d2c8aa84b5e9b421d174aaa4bb81b91738e5204
Marco Gulino <marco@gulinux.net>
2019-06-09 17:12:31 +0100

    Fix Debian Stretch and Buster dependencies

--- support/docker/files/configuration-debian-buster.cmake
+++ support/docker/files/configuration-debian-buster.cmake
@@ -1,3 +1,3 @@
 include(${CMAKE_CURRENT_LIST_DIR}/configuration-debian-base.cmake)
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libusb-1.0-0, libqt5gui5 (>= 5.5.0), libqt5opengl5 (>= 5.5.0), libqt5network5 (>= 5.5.0), libqt5widgets5 (>= 5.5.0), libopencv-imgproc3.2, libopencv-core3.2, libopencv-highgui3.2, fxload, libccfits0v5" CACHE STRING "")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libusb-1.0-0, libqt5gui5 (>= 5.5.0), libqt5opengl5 (>= 5.5.0), libqt5network5 (>= 5.5.0), libqt5widgets5 (>= 5.5.0), libopencv-imgproc3.2, libopencv-core3.2, libopencv-highgui3.2, fxload, libccfits0v5, libopencv-shape3.2, libopencv-stitching3.2, libopencv-superres3.2, libopencv-videostab3.2, libopencv-contrib3.2, libqt5qml5" CACHE STRING "")
 set(EXTRA_LIBRARIES udev CACHE STRING "")
--- support/docker/files/configuration-debian-stretch.cmake
+++ support/docker/files/configuration-debian-stretch.cmake
@@ -1,3 +1,3 @@
 include(${CMAKE_CURRENT_LIST_DIR}/configuration-debian-base.cmake)
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libusb-1.0-0, libqt5gui5 (>= 5.5.0), libqt5opengl5 (>= 5.5.0), libqt5network5 (>= 5.5.0), libqt5widgets5 (>= 5.5.0), libopencv-imgproc2.4v5, libopencv-core2.4v5, libopencv-highgui2.4-deb0, fxload, libccfits0v5" CACHE STRING "")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libqt5gui5 (>= 5.5.0), libqt5opengl5 (>= 5.5.0), libqt5network5 (>= 5.5.0), libqt5widgets5 (>= 5.5.0), libopencv-imgproc2.4v5, libopencv-core2.4v5, libopencv-highgui2.4-deb0, fxload, libccfits0v5, libopencv-videostab2.4v5, libopencv-ts2.4v5, libopencv-superres2.4v5, libopencv-stitching2.4v5, libopencv-contrib2.4v5, libqt5qml5" CACHE STRING "")
 set(EXTRA_LIBRARIES udev CACHE STRING "")
