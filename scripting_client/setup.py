6871f96007525eddf8565acf819ce246a80c41ad
Marco Gulino <marco@gulinux.net>
2019-06-10 20:42:43 +0100

    Change website and application logo (#88)
    * Change website and application logo
    
    * Add travis badge to README

--- scripting_client/setup.py
+++ scripting_client/setup.py
@@ -4,7 +4,7 @@ setup(
     name='planetaryimager_scripting',
     version='1.0',
     packages=['planetaryimager', 'planetaryimager.utils', 'planetaryimager.network'],
-    url='http://blog.gulinux.net/planetary-imager',
+    url='http://planetaryimager.gulinux.net',
     license='GPLv3',
     author='Marco Gulino',
     author_email='marco@gulinux.net',
